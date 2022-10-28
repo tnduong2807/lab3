d757a4cfc562e59447b1398795095f6a47481a48
tnduong2807 <tnduong@ucsd.edu>
2022-10-28 00:14:29 -0700

    new

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,4 +1,5 @@
-
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ArrayExamples {
 
@@ -39,12 +40,13 @@ public class ArrayExamples {
   public static void main(String[] args) {
     int[] x = new int[]{1, 2, 3, 4, 5};
     double[] y = new double[]{};
-
+    /*
     for (int i = 0; i < x.length; i++) {
       System.out.println(averageWithoutLowest(y));
     }
-
-    
+    */
+    reverseInPlace(x);
+    System.out.println(Arrays.toString(x));
 
   }
 
